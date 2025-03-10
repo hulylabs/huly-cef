550c77ce72fc393e02abaee573c32c33e8415d67
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-03-10 20:52:02 +1100

    add MacOS build script
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
+
 use anyhow::Result;
-use cef_ui_util::link_cef;
+use cef_ui_util::{link_cef, link_cef_helper};
 
 fn main() -> Result<()> {
     link_cef()?;
+    link_cef_helper()?;
+
     Ok(())
 }
