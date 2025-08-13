6dd60e85bbc536bcf322f3af9c55c09e012fd11a
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-08-13 18:42:48 +1100

    add a new version of huly-cef-helper that works
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- crates/huly-cef-helper/src/main.rs
+++ crates/huly-cef-helper/src/main.rs
@@ -1,41 +1,21 @@
+use std::{env::current_exe, ffi::{c_int, c_void}, fs::canonicalize, path::PathBuf, process::exit, ptr::null_mut};
 
-use anyhow::{anyhow, Result};
-use cef_ui::{AppCallbacks, RenderProcessHandlerCallbacks};
-use cef_ui_helper::{cef_main_args_t, MainArgs, ScopedSandbox};
-use cef_ui_sys::cef_app_t;
+use anyhow::Result;
+use anyhow::anyhow;
+use cef_ui::{AppCallbacks, MainArgs, RenderProcessHandler, RenderProcessHandlerCallbacks};
+use cef_ui_helper::ScopedSandbox;
+use cef_ui_sys::{cef_app_t, cef_main_args_t};
 use libloading::{Library, Symbol};
-use log::{error, info, SetLoggerError};
-use log4rs::{append::{console::ConsoleAppender, file::FileAppender}, config::{Appender, Root}, encode::pattern::PatternEncoder, Config};
-use std::{
-    env::current_exe,
-    ffi::{c_int, c_void},
-    fs::canonicalize,
-    path::PathBuf,
-    process::exit,
-    ptr::null_mut
-};
+use tracing::{error, info, level_filters::LevelFilter, subscriber::set_global_default, Level};
+use tracing_log::{ LogTracer};
+use tracing_subscriber::FmtSubscriber;
 
+fn main() {
+    run(true);
+}
 
 /// The relative path to the CEF framework library within the app bundle on macOS.
 const CEF_PATH: &str = "../../../Chromium Embedded Framework.framework/Chromium Embedded Framework";
-
-fn setup_logging() -> Result<log4rs::Handle, SetLoggerError> {
-    let stdout_pattern = "\x1b[90m{d(%H:%M:%S%.3f)} \x1b[0m{h({l})} \x1b[90m{f}:{L} \x1b[0m{m}{n}";
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(stdout_pattern)))
-        .build();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(
-            Root::builder()
-                .appender("stdout")
-                .build(log::LevelFilter::Info),
-        )
-        .unwrap();
-
-    log4rs::init_config(config)
-}
 
 /// Returns the CEF error code or 1 if an error occurred.
 pub fn run(sandbox: bool) {
@@ -52,15 +32,21 @@ pub fn run(sandbox: bool) {
 
 /// Try and run the helper, returning the CEF error code if successful.
 fn try_run(sandbox: bool) -> Result<i32> {
-    setup_logging()?;
+    // This routes log macros through tracing.
+    LogTracer::init()?;
+
+    // Setup the tracing subscriber globally.
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(LevelFilter::from_level(Level::DEBUG))
+        .finish();
+
+    set_global_default(subscriber)?;
 
     // Setup the sandbox if enabled.
     let _sandbox = match sandbox {
         true => Some(ScopedSandbox::new()?),
         false => None
     };
-
-    let app = cef_ui::App::new(HelperAppCallbacks {});
 
     // Manually load CEF and execute the subprocess.
     let ret = unsafe {
@@ -73,12 +59,14 @@ fn try_run(sandbox: bool) -> Result<i32> {
         let cef_path = get_cef_path(CEF_PATH)?;
         let lib = Library::new(cef_path)?;
 
-        // Manually load the cef_execute_process function.
+
         let cef_execute_process: Symbol<
             unsafe extern "C" fn(args: *const cef_main_args_t, *mut cef_app_t, *mut c_void) -> c_int
         > = lib.get(b"cef_execute_process")?;
 
         info!("Executing CEF subprocess ..");
+
+        let app = cef_ui::App::new(HelperAppCallbacks);
 
         // Execute the CEF subprocess.
         let ret = cef_execute_process(main_args.as_raw(), app.into_raw(), null_mut()) as i32;
@@ -108,29 +96,40 @@ fn get_cef_path(relative_path: &str) -> Result<PathBuf> {
     Ok(cef_path)
 }
 
+struct HelperAppCallbacks;
 
-fn main() {
-    run(true);
+impl AppCallbacks for HelperAppCallbacks {
+    fn on_before_command_line_processing(
+        &mut self,
+        _: Option<&str>,
+        _: Option<cef_ui::CommandLine>
+    ) {
+        
+    }
+
+    fn get_browser_process_handler(&mut self) -> Option<cef_ui::BrowserProcessHandler> {
+        None
+    }
+
+    fn get_render_process_handler(&mut self) -> Option<cef_ui::RenderProcessHandler> {
+        Some(RenderProcessHandler::new(RenderProcessCallbacks))
+    }
 }
 
+struct RenderProcessCallbacks;
 
-struct HelperRenderProcessCallbacks;
-
-impl RenderProcessHandlerCallbacks for HelperRenderProcessCallbacks {
+impl RenderProcessHandlerCallbacks for RenderProcessCallbacks {
     fn on_web_kit_initialized(&mut self) {
-        info!("on_web_kit_initialized");
+        info!("[on_web_kit_initialized]");
     }
 
     fn on_browser_created(&mut self, browser: cef_ui::Browser, extra_info: Option<cef_ui::DictionaryValue>) {
-        info!("on_browser_created");
     }
 
     fn on_browser_destroyed(&mut self, browser: cef_ui::Browser) {
-        info!("on_browser_destroyed");
     }
 
     fn on_context_created(&mut self, browser: cef_ui::Browser, frame: cef_ui::Frame, context: cef_ui::V8Context) {
-        info!("on_context_created");
     }
 
     fn on_process_message_received(
@@ -140,28 +139,6 @@ impl RenderProcessHandlerCallbacks for HelperRenderProcessCallbacks {
         source_process: cef_ui::ProcessId,
         message: &mut cef_ui::ProcessMessage
     ) -> bool {
-        info!("on_process_message_received");
-        // Handle the message here if needed.
-        false // Return true if the message was handled, false otherwise.
-    }
-}
-struct HelperAppCallbacks;
-
-impl AppCallbacks for HelperAppCallbacks {
-    fn on_before_command_line_processing(
-        &mut self,
-        process_type: Option<&str>,
-        command_line: Option<cef_ui::CommandLine>
-    ) {
-        info!("on_before_command_line_processing");
-    }
-
-    fn get_browser_process_handler(&mut self) -> Option<cef_ui::BrowserProcessHandler> {
-        info!("get_browser_process_handler");
-        None
-    }
-
-    fn get_render_process_handler(&mut self) -> Option<cef_ui::RenderProcessHandler> {
-        Some(cef_ui::RenderProcessHandler::new(HelperRenderProcessCallbacks {}))
+        true
     }
 }
