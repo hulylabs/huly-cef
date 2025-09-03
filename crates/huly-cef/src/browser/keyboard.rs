0b3448c7f4261d6f991494bc2f182e61e7b09699
NikitaSkrynnik <nikita.skrynnik@xored.com>
2025-09-03 17:30:27 +1100

    fix windows keyboard
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- crates/huly-cef/src/browser/keyboard.rs
+++ crates/huly-cef/src/browser/keyboard.rs
@@ -92,6 +92,67 @@ fn process_key_event(
     }
 }
 
+#[cfg(target_os = "windows")]
+fn process_key_event(
+    host: &BrowserHost,
+    character: u16,
+    windowscode: i32,
+    code: i32,
+    down: bool,
+    ctrl: bool,
+    shift: bool,
+) {
+    let mut modifiers = EventFlags::empty();
+    if ctrl {
+        modifiers = modifiers | EventFlags::ControlDown;
+    }
+    if shift {
+        modifiers = modifiers | EventFlags::ShiftDown;
+    }
+
+    if down {
+        let event = KeyEvent {
+            event_type: KeyEventType::RawKeyDown,
+            modifiers,
+            windows_key_code: windowscode.into(),
+            native_key_code: code,
+            is_system_key: false,
+            character: 0,
+            unmodified_character: 0,
+            focus_on_editable_field: false,
+        };
+
+        _ = host.send_key_event(event);
+
+        if character > 0 {
+            let char_event = KeyEvent {
+                event_type: KeyEventType::Char,
+                modifiers,
+                windows_key_code: (character as i32).into(),
+                native_key_code: 0,
+                is_system_key: false,
+                character: 0,
+                unmodified_character: 0,
+                focus_on_editable_field: false,
+            };
+            _ = host.send_key_event(char_event);
+        }
+    } else {
+        let up_event = KeyEvent {
+            event_type: KeyEventType::KeyUp,
+            modifiers,
+            windows_key_code: windowscode.into(),
+            native_key_code: code,
+            is_system_key: false,
+            character: 0,
+            unmodified_character: 0,
+            focus_on_editable_field: false,
+        };
+        _ = host.send_key_event(up_event);
+    }
+}
+
+#[allow(dead_code)]
 fn to_lower_case(unicode: u16) -> u16 {
     char::from_u32(unicode as u32)
         .expect("invalid unicode character")
