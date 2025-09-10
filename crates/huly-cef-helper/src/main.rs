3b65de109373548065a868c8e8c1ef7d83c5052b
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-09-10 19:32:59 +1100

    move all cef abstractions to a cef_lib.rs file in huly-cef-helper
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- crates/huly-cef-helper/src/main.rs
+++ crates/huly-cef-helper/src/main.rs
@@ -1,9 +1,8 @@
-use std::{process::exit, ptr::null_mut, mem::zeroed, ffi::c_char};
+use std::{process::exit, ptr::null_mut};
 
 use anyhow::Result;
 use cef_ui::{App, AppCallbacks, MainArgs, RenderProcessHandler, SchemeOptions, SchemeRegistrar};
 use cef_ui_helper::ScopedSandbox;
-use cef_ui_sys::cef_string_t;
 use log::{error, info, SetLoggerError};
 use log4rs::{
     append::console::ConsoleAppender,
@@ -13,40 +12,11 @@ use log4rs::{
 };
 
 use crate::render_process::RenderProcessCallbacks;
+use crate::cef_lib::SchemeRegistrarExt;
 
 mod cef_lib;
 mod js;
 mod render_process;
-
-unsafe fn new_cef_string(s: &str) -> cef_string_t {
-    let lib = &cef_lib::CEFLIB;
-    let mut ret: cef_string_t = unsafe { zeroed() };
-
-    if s.is_empty() {
-        return ret;
-    }
-
-    match (lib.cef_string_utf8_to_utf16)(s.as_ptr() as *const c_char, s.len(), &mut ret) {
-        0 => panic!("Failed to convert from utf8 to utf16!"),
-        _ => ret
-    }
-}
-
-trait SchemeRegistrarExt {
-    fn add_custom_scheme_raw(&self, scheme_name: &str, options: i32) -> Result<bool>;
-}
-
-impl SchemeRegistrarExt for SchemeRegistrar {
-    fn add_custom_scheme_raw(&self, scheme_name: &str, options: i32) -> Result<bool> {
-        unsafe {
-            if let Some(add_custom_scheme) = (*self.0).add_custom_scheme {
-                Ok(add_custom_scheme(self.0, &new_cef_string(scheme_name), options) != 0)
-            } else {
-                Err(anyhow::anyhow!("add_custom_scheme is None"))
-            }
-        }
-    }
-}
 
 fn setup_logging() -> Result<log4rs::Handle, SetLoggerError> {
     let stdout_pattern = "\x1b[90m{d(%H:%M:%S%.3f)} \x1b[0m{h({l})} \x1b[90m{f}:{L} \x1b[0m{m}{n}";
