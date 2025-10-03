23d484bbb6c0d3e69a50d288543dfeb57fb93118
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-10-03 11:54:47 +0200

    add better names for downloaded files
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- crates/huly-cef/src/browser/client/download_callbacks.rs
+++ crates/huly-cef/src/browser/client/download_callbacks.rs
@@ -11,6 +11,19 @@ impl MyDownloadHandlerCallbacks {
     }
 }
 impl DownloadHandlerCallbacks for MyDownloadHandlerCallbacks {
+    fn on_before_download(
+        &mut self,
+        _: Browser,
+        _: DownloadItem,
+        suggested_name: &str,
+        callback: cef_ui::BeforeDownloadCallback,
+    ) -> bool {
+        let full_path = get_file_path(suggested_name);
+        callback
+            .continue_download(Some(&full_path), false)
+            .expect("failed to continue download");
+        true
+    }
     fn on_download_updated(
         &mut self,
         _: Browser,
@@ -22,9 +35,9 @@ impl DownloadHandlerCallbacks for MyDownloadHandlerCallbacks {
             if s.downloads.contains_key(&id) {
                 return;
             }
-
             s.downloads.insert(id, callback);
         });
+
         let path = download_item
             .get_full_path()
             .expect("failed to get full path");
@@ -34,7 +47,6 @@ impl DownloadHandlerCallbacks for MyDownloadHandlerCallbacks {
         let total = download_item
             .get_total_bytes()
             .expect("failed to get total bytes") as u64;
-
         let is_complete = download_item
             .is_complete()
             .expect("failed to check if download is complete");
@@ -52,4 +64,28 @@ impl DownloadHandlerCallbacks for MyDownloadHandlerCallbacks {
             is_aborted,
         });
     }
+}
+
+fn get_file_path(name: &str) -> String {
+    let download_dir = dirs::download_dir().expect("failed to get download dir");
+    let mut full_path = download_dir.join(name);
+
+    let mut counter = 1;
+    while full_path.exists() {
+        let stem = std::path::Path::new(name)
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .expect("invalid file name");
+        let extension = std::path::Path::new(name)
+            .extension()
+            .and_then(|s| s.to_str())
+            .map(|s| format!(".{}", s))
+            .expect("invalid file extension");
+
+        let new_name = format!("{} ({}){}", stem, counter, extension);
+        full_path = download_dir.join(new_name);
+        counter += 1;
+    }
+
+    return full_path.to_str().unwrap_or(name).to_string();
 }
